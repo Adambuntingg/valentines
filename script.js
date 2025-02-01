aa43656cbb0bea91affa990c752080140e9113bc
Adambuntingg <156615419+Adambuntingg@users.noreply.github.com>
2025-02-01 20:07:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function checkName() {
-    const correctName = "Alice"; // Change this to your preferred name
+    const correctName = "Elsa"; // Change this to your preferred name
     const enteredName = document.getElementById("nameInput").value.trim();
 
     if (enteredName === correctName) {
         window.location.href = "secret.html";
     } else {
-        alert("Incorrect name, try again!");
+        alert("You arent my vals girl, go away!");
     }
 }
